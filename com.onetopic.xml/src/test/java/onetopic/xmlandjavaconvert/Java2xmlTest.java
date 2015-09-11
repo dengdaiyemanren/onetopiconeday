57a59368d0de1a372c221f2d47f36861f45cc60c
dengdaiyemanren <315531514@qq.com>
2015-09-11 23:51:24 +0800

    关闭流文件

--- com.onetopic.xml/src/test/java/onetopic/xmlandjavaconvert/Java2xmlTest.java
+++ com.onetopic.xml/src/test/java/onetopic/xmlandjavaconvert/Java2xmlTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.math.BigDecimal;
 
@@ -17,7 +18,7 @@ import org.junit.Test;
 public class Java2xmlTest
 {
     @Test
-    public void javaToxmlFile() throws FileNotFoundException, JAXBException
+    public void javaToxmlFile() throws JAXBException
     {
         Product object = new Product();
         object.setCode("WI1");
@@ -26,19 +27,62 @@ public class Java2xmlTest
         
         String filePath = this.getClass().getResource("xml/").getPath() + "javaXml.xml";
         
-        OutputStream os = new FileOutputStream(filePath);
-        new Java2xml().convertToXml(object, os);
+        OutputStream os = null;
+        try
+        {
+            os = new FileOutputStream(filePath);
+            new Java2xml().convertToXml(object, os);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (null != os)
+            {
+                try
+                {
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                }
+            }
+        }
+        
     }
     
     @Test
-    public void javaToxmlSytemOut() throws FileNotFoundException, JAXBException
+    public void javaToxmlSytemOut() throws JAXBException
     {
         Product object = new Product();
         object.setCode("WI1");
         object.setName("Widget Number One");
         object.setPrice(BigDecimal.valueOf(300.00));
-        
-        new Java2xml().convertToXml(object, System.out);
+        OutputStream os = null;
+        try
+        {
+            os = System.out;
+            new Java2xml().convertToXml(object, System.out);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (os != null)
+            {
+                try
+                {
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                }
+            }
+        }
         
     }
     
