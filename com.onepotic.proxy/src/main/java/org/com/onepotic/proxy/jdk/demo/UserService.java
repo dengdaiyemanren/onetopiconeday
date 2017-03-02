033ad816c46d5709b868b5528daffd1a2f74d458
dengdaiyemanren <315531514@qq.com>
2017-03-02 21:31:30 +0800

    proxy demo

---
+++ com.onepotic.proxy/src/main/java/org/com/onepotic/proxy/jdk/demo/UserService.java
@@ -0,0 +1,15 @@
+package org.com.onepotic.proxy.jdk.demo;
+
+/**
+ * 目标对象实现的接口，用JDK来生成代理对象一定要实现一个接口
+ * @author yinlg
+ * @created 2017年3月2日 下午9:29:40
+ */
+public interface UserService {
+
+	/**
+	 * 目标方法 
+	 */
+	public abstract void add();
+
+}
---
+++ com.onepotic.proxy/src/main/java/org/com/onepotic/proxy/jdk/demo/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package org.com.onepotic.proxy.jdk.demo;
+
+/**
+ * 目标对象
+ * @author yinlg
+ * @created 2017年3月2日 下午9:30:42
+ */
+public class UserServiceImpl implements UserService {
+
+	/* (non-Javadoc)
+	 * @see dynamic.proxy.UserService#add()
+	 */
+	public void add() {
+		System.out.println("--------------------add---------------");
+	}
+}
