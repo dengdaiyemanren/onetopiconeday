aab21d43f00b3263030bc7b6005131b96e6aae33
dengdaiyemanren <315531514@qq.com>
2017-03-02 21:39:04 +0800

    proxy demo test

---
+++ com.onepotic.proxy/src/test/java/org/com/onepotic/proxy/jdk/demo/ProxyTest.java
@@ -0,0 +1,22 @@
+package org.com.onepotic.proxy.jdk.demo;
+
+import org.junit.Test;
+
+public class ProxyTest {
+
+	@Test
+	public void testProxy() throws Throwable {
+		// 实例化目标对象
+		UserService userService = new UserServiceImpl();
+		
+		// 实例化InvocationHandler
+		MyInvocationHandler invocationHandler = new MyInvocationHandler(userService);
+		
+		// 根据目标对象生成代理对象
+		UserService proxy = (UserService) invocationHandler.getProxy();
+		
+		// 调用代理对象的方法
+		proxy.add();
+		
+	}
+}
